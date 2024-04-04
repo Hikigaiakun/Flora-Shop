a0ce26183e1bdef89b3301c97882541bd32ba050
Hikigaiakun <165752796+Hikigaiakun@users.noreply.github.com>
2024-04-04 12:03:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 window.addEventListener('scroll', function () {
-	var scroll = document.querySelector('.btn-upper')
+	var scroll = document.querySelector('#btn-upper')
 	scroll.classList.toggle('active', window.scrollY > 500)
 })
 
-document.querySelector('.btn-upper').addEventListener('click', function () {
+document.querySelector('#btn-upper').addEventListener('click', function () {
 	window.scrollTo({
 		top: 0,
 		behavior: 'smooth',
